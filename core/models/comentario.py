82d609894dd78f1031d232e8d3b5fd7e617f74f8
spricigom <emilispricigo.ifc@gmail.com>
2025-06-13 15:16:22 -0300

    fix: correção de relação de tabelas

--- core/models/comentario.py
+++ core/models/comentario.py
@@ -5,7 +5,7 @@ from .resenha import Resenha
 
 class Comentario(models.Model):
     texto = models.CharField(max_length=250, null=True, blank=True)
-    resenha = models.ForeignKey(Resenha, on_delete=models.PROTECT, related_name="Comentario_resenha", null=True, blank=True)
+    resenha = models.ForeignKey(Resenha, on_delete=models.PROTECT, related_name="Comentario_resenha")
 
     def __str__(self):
         return self.resenha.titulo
--- core/models/curtida.py
+++ core/models/curtida.py
@@ -5,7 +5,7 @@ from .resenha import Resenha
 
 class Curtida(models.Model):
     curtida = models.BooleanField()
-    resenha = models.ForeignKey(Resenha, on_delete=models.PROTECT, related_name="Curtida_resenha", null=True, blank=True)
+    resenha = models.ForeignKey(Resenha, on_delete=models.PROTECT, related_name="Curtida_resenha")
 
     def __str__(self):
         return self.resenha.titulo
