c6d8a75b26e8a6c696a120ad6a11689690918c61
spricigom <emilispricigo.ifc@gmail.com>
2025-06-13 14:24:09 -0300

    fix: correções feitas

--- core/models/curtida.py
+++ core/models/curtida.py
@@ -5,6 +5,7 @@ from .resenha import Resenha
 
 class Curtida(models.Model):
     curtida = models.BooleanField()
-    resenha = models.ForeignKey(Resenha, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
+    resenha = models.ForeignKey(Resenha, on_delete=models.PROTECT, related_name="Curtida_resenha", null=True, blank=True)
+
     def __str__(self):
         return f"{Resenha.titulo}"
